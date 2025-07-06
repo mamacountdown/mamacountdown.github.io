79aca0670a3b5cc0435076a23292b0fc4197960b
mamacountdown <bloonsalwayssuck@proton.me>
2025-07-06 12:03:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
+let dayField = document.getElementById('day');
+let hourField = document.getElementById('hour');
+let minuteField = document.getElementById('minute');
+let secondField = document.getElementById('second');
+
 let interval;
 const eventDay = new Date(Date.parse("2025-07-16T22:30:00"));
-
+  
+// Convert to millisecond
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
-
+  
 const countDownFn = () => {
-  everySecond = setInterval(countDownFn, second)
-  everyMinute = setInterval(countDownFn, minute)
-  everyHour = setInterval(countDownFn, hour)
-  let now = new Date();
-  let timeSpan = eventDay - now;
-}
-
-if (timeSpan <= -today) {
-  console.log("Sind wir da?");
-  clearInterval(interval);
-}
-else {
-  const days = Math.floor(timeSpan / day)
-  const hours = Math.floor((timeSpan % day) / hour)
-  const minutes = Math.floor((timeSpan % hour) / minute)
-  const seconds = Math.floor((timeSpan % minute) / second)
-
-  console.log(days + ":" + hours + ":" + minutes + ":" + seconds);
-}
+    const today = moment();
+    const timeSpan = eventDay.diff(today);
+  
+    if (timeSpan <= -today) {
+        console.log("Unfortunately we have past the event day");
+        clearInterval(interval);
+        return;
+    } else if (timeSpan <= 0) {
+        console.log("Today is the event day");
+        clearInterval(interval);
+        return;
+    } else {
+        const days = Math.floor(timeSpan / day);
+        const hours = Math.floor((timeSpan % day) / hour);
+        const minutes = Math.floor((timeSpan % hour) / minute);
+        const seconds = Math.floor((timeSpan % minute) / second);
+  
+        // Set results
+        dayField.innerHTML = days;
+        hourField.innerHTML = hours;
+        minuteField.innerHTML = minutes;
+        secondField.innerHTML = seconds;
+    }
+};
+  
+interval = setInterval(countDownFn, second);
 
